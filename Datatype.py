abdf4c7ef7cd7e5c4beb2c3817fdaa5ac8118f89
Chintan Suthar <csre7144@gmail.com>
2025-05-18 19:17:39 +0530

    Variable and DataType

---
+++ Datatype.py
@@ -0,0 +1,10 @@
+a = 30 # a is an integer
+
+b = 5.22 # b is a floating point number
+
+c = "Data Engineering" # c is a boolean variable
+
+d = False and True # d is a boolean variable
+
+e = None # e is a None type Variable
+
---
+++ Variable_and_Datetype.py
@@ -0,0 +1,27 @@
+# A varibale is the name given to a memory location in a program. For example
+
+# Practice 01
+# a = 30 
+# b = "Data Engineering"
+# c= 19.20
+# print(a)
+# print(b)
+# print(c)
+
+# Practice 02
+a = 10
+b = 5
+print(a+b) #Addition
+print(a-b) #Subtraction
+print(a/b) #Mu
+
+
+# Rule Variable
+# @chintz = 56 #Invaild due to @ symbol 
+# @chintz #Invaild due to @ symbol 
+
+aaa = 4545
+print(aaa)
+
+_aaa = 45454
+print(_aaa)
