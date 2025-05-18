34c1799113a2cb7d38e1b6fd049b1e7b2653ba80
Chintan Suthar <csre7144@gmail.com>
2025-05-18 18:33:47 +0530

    Type Of Comments

---
+++ type_of_comments.py
@@ -0,0 +1,16 @@
+# # Type Of Comments 
+# 1. Single Line Comments:- To write a single line comment just add "#" at the start of the line
+# 2. Multiple Comments:- To write Multiple Comments you can use "#" at each line or you can use the mulitple string (""" """)
+
+# Let's practical the below code 
+
+
+# 1.Single Line
+# a = 'hello world'
+a = "Hello World"
+print(a)
+
+# 2. Multiple Comment 
+b= '''Hello World
+'''
+print(b)
