36f22ddfc53f484cedbe56d3364213c19d73cb1d
Chintan Suthar <csre7144@gmail.com>
2025-05-20 09:23:07 +0530

    String

---
+++ string.py
@@ -0,0 +1,12 @@
+a = 'hello' #Single Quote String
+b = "hello" #Double Quote String
+c = '''hello''' #Triple Quote String
+
+print(a)
+print(b)
+print(c)
+
+
+name = "India"  #Start from index 0 all the way till 3 (excluding 3)
+nameshort = name[2:4]
+print(nameshort)
