085760a6dc10d8438fa40c5ff73019a91f1c2069
Chintan Suthar <csre7144@gmail.com>
2025-06-04 09:09:22 +0530

    Lists and Tuples

---
+++ list.py
@@ -0,0 +1,13 @@
+first = ["apple", "banana", "orange", 5, 3.456, False, "Chintan", "Piryam"]
+print(first)
+print(first[0])
+ 
+first[0] = "Hello" #Unlike Strings lists are mutable
+print(first[0])
+
+# practice set
+L1 = [7,9,"Akashy"]
+print(L1[0]) #7 
+print(L1[1]) #9
+# print(L1[70]) #error
+print(L1[0:2]) #list slicing
---
+++ list_method.py
@@ -0,0 +1,30 @@
+first = ["apple", "banana", "orange", 5, 3.456, False, "Chintan", "Piryam"]
+
+# append
+first.append("Harshiv")
+print(first)
+
+# sort
+L1 = [1,6,34,62,2,11]
+L1.sort()
+print(L1)
+
+# reverse
+L1 = [1,6,34,62,2,11]
+L1.reverse()
+print(L1)
+
+# insert
+L1 = [1,6,34,62,2,11]
+L1.insert(14, 333) #Insert such that its index in the lise is 3
+print(L1)
+
+# pop
+L1 = [1,6,34,62,2,11]
+L1.pop(2) #pop will delete element at index 2 and return its value
+print(L1)
+
+# Remove
+L1 = [1,6,34,62,2,11]
+L1.remove(62) #will remove 62 from the list
+print(L1)
