8b458e2c53fbfdc2cd2d8e28c09a04ea10579445
Chintan Suthar <csre7144@gmail.com>
2025-06-05 09:20:47 +0530

    Condition Method

---
+++ Condition_Method/02_condition.py
@@ -0,0 +1,22 @@
+# Multiple Condition Method
+
+a = int(input("Enter your age: "))
+
+# If else ladder
+if (a%2 == 0):
+    print("A is even")
+
+if (a>18):
+    print("You are above the age of consent")
+    print("Good for you")
+
+elif(a<0):
+    print("You are entering an invaild negative age")
+
+elif(a==0):
+    print("You are entering o which is not a vaild age")
+
+else:
+    print("You are below the age of consent")
+
+print("End of program")
