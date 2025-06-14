0bb033b7d46c14392dae340b326c34242b6de003
Chintan Suthar <csre7144@gmail.com>
2025-06-14 12:46:10 +0530

    Function Method

---
+++ function_method/01_function.py
@@ -0,0 +1,25 @@
+# a = 12
+# b = 45
+# c= 56
+
+# average = (a+b+c)/3
+# print(average)
+
+
+# a = int(input("Enter your number A:"))
+# b = int(input("Enter your number B:"))
+# c= int(input("Enter your number C:"))
+
+# average = (a+b+c)/3
+# print(average)
+
+def avg():
+    a = int(input("Enter your number A:"))
+    b = int(input("Enter your number B:"))
+    c= int(input("Enter your number C:"))
+
+    average = (a+b+c)/3
+    print(average)
+
+avg()
+print("Thank you!")
---
+++ function_method/02_function_with_argument.py
@@ -0,0 +1,7 @@
+def goodDay(name, ending):
+    print("Good Day, " + name)
+    print(ending)
+    # return "OK"
+    return 7
+a = goodDay("Chintu", "Thank you")
+print(a)
