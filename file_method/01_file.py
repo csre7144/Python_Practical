38e14f7f479300e1e0373e87f0ad32811ba0540e
Chintan Suthar <csre7144@gmail.com>
2025-06-16 09:15:49 +0530

    File Method

---
+++ file_method/01_file.py
@@ -0,0 +1,15 @@
+'''
+a = "A very long string with emails"
+
+emails =[]
+3 seconds
+'''
+
+# f = open("file.txt")
+# data = f.read()
+# print(data)
+# f.close()
+f = open("d:/Python Roadmap 2025/Python Practical/file_method/file.txt")
+data = f.read()
+print(data)
+f.close()
---
+++ file_method/02_file_write.py
@@ -0,0 +1,5 @@
+st = "Hey Chintar, You are amazing."
+
+f = open("d:/Python Roadmap 2025/Python Practical/file_method/myfile.txt", "w")
+f.write(st)
+f.close()
