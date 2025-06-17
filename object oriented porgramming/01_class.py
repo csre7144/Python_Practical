77fdd5b32026f71f1cbf412f10c308330b6e85e6
Chintan Suthar <csre7144@gmail.com>
2025-06-17 09:17:15 +0530

    Object Oriented Programming

---
+++ object oriented porgramming/01_class.py
@@ -0,0 +1,23 @@
+# class Employee:
+#     name = "chintan"
+#     language = "Py"
+#     salary = 1500
+
+# st = Employee()
+# print(st.name, st.language)
+
+# Output: chintan Py
+
+
+class Employee:
+    name = "chintan" # Specific to each class
+
+st = Employee() # Object Instatiation 
+st.name
+Employee.name = "Harshiv" # Changing class attribute
+print(st.name) 
+# Output: Harshiv
+
+st.salary = "40K" # Adding isntance attribute
+print(st.name, st.salary)
+# Output: Harshiv 40K
