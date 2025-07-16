1561f72f6684e879b134dfcc8fee72a7fa2211f3
Chintan Suthar <csre7144@gmail.com>
2025-07-16 09:15:54 +0530

    Enumerate

---
+++ Advance Python/Walruns/11_enumerate.py
@@ -0,0 +1,29 @@
+# Practice 01:
+
+# l = [3,55,130,150]
+
+# index = 0
+# for item in l:
+#     index += 1
+#     print(f"The item number {index} is {item}")
+
+# Output:-
+# The item number 1 is 3
+# The item number 2 is 55
+# The item number 3 is 130
+# The item number 4 is 150
+
+# Practice 02:
+
+# This can be simplified using enumerate function
+l = [3,55,130,150]
+
+index = 0
+for index, item in enumerate(l):
+    print(f"The item number {index} is {item}")
+
+# Output:-
+# The item number 1 is 3
+# The item number 2 is 55
+# The item number 3 is 130
+# The item number 4 is 150
