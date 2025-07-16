ed429f4950b25553aff930ee931308369a280c67
Chintan Suthar <csre7144@gmail.com>
2025-07-16 09:14:32 +0530

    Main Method

---
+++ Advance Python/Walruns/09_main.py
@@ -0,0 +1,18 @@
+# def main():
+#     try:
+#         a = int(input("Hey, Enter a number: "))
+#         print(a)
+
+#     except Exception as e:
+#         print(e)
+
+#     finally:
+#         print("I am inside finally")
+
+# main()
+# Output:
+# 55
+# I am inside finally
+
+
+from module import myFunc
